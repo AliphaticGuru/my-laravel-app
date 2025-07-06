c43b385be7aea24afe9c1dbfe0ae5ae9c0840682
Aliphaticguru <thegen@Administrator.localdomain>
2025-07-06 01:50:26 +0100

    second.02 commit

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,78 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
+//  Index
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(5);
+    $jobs = Job::with('employer')->latest()->simplePaginate(5);
     
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
+//  Create
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
 
-    return view('job', ['job' => $job]);
+//  Show
+Route::get('/jobs/{job}', function (Job $job) {
+    // $job = Job::find($id);
+
+    return view('jobs.show', ['job' => $job]);
+});
+
+//  Store
+Route::post('/jobs', function () {
+    // dd(request('title'));
+    request()->validate([
+        'title' =>  ['required', 'min:3'],
+        'salary'    => ['required']
+        
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+//  Edit
+Route::get('/jobs/{job}/edit', function (Job $job) {
+    // $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//  Update
+Route::patch('/jobs/{job}', function (Job $job) {
+
+    request()->validate([
+        'title' =>  ['required', 'min:3'],
+        'salary'    => ['required']   
+    ]);
+        // $job->title = request('title');
+        // $job->salary = request('salary');
+        // $job->save();
+    // $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+//  Destroy
+Route::delete('/jobs/{id}', function (Job $job) {
+    // Job::findOrFail($id)->delete();
+    $job->delete();
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
