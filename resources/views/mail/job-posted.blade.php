7b13dc37735105d014f299d0cd2b1e2752a83dad
Aliphaticguru <thegen@Administrator.localdomain>
2025-07-11 05:44:07 +0100

    emailing using mailable classes

---
+++ resources/views/mail/job-posted.blade.php
@@ -0,0 +1,14 @@
+<h2>
+    {{ $job->title }}
+</h2>
+<p>
+    Congratulations! You have successfully completed the task.
+</p>
+
+<p>
+    <a href="{{ url('/jobs/' . $job->id)}}">View Your Job Listing</a>
+</p>
+
+<div>
+    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+</div>
